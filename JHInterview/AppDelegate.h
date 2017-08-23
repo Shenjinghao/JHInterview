af57e2ea21e75560685abddae7e60e1a8cd95391
shenjinghao <shenjinghao@ixiaocong.com>
2017-08-23 18:13:21 +0800

    Initial Commit

---
+++ JHInterview/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JHInterview
+//
+//  Created by jinghao shen on 2017/8/23.
+//  Copyright © 2017年 jinghao shen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JHInterview/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JHInterview
+//
+//  Created by jinghao shen on 2017/8/23.
+//  Copyright © 2017年 jinghao shen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
